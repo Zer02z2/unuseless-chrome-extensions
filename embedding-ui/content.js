91a3d5ef7e7c7057d50a035cb5769b5274f9b832
Zer02z2 <zongzechan@gmail.com>
2024-11-13 19:17:35 -0500

    clean up text

--- embedding-ui/content.js
+++ embedding-ui/content.js
@@ -16,14 +16,43 @@ const fetchEmbedding = async (input, length) => {
     console.log("start fetching")
     const response = await fetch(url, options)
     const parsedResponse = await response.json()
-    console.log(parsedResponse)
+    return parsedResponse
   } catch (error) {
     console.log(error)
   }
 }
 
+const elementsNodes = document.body.querySelectorAll("*")
+const elements = Array.from(elementsNodes)
+
+const textElements = elements.filter((element) => {
+  const childNodes = element.childNodes
+  return Array.from(childNodes).some(
+    (child) =>
+      child.nodeType === Node.TEXT_NODE && child.textContent.trim() !== ""
+  )
+})
+
+const textContents = textElements.map((element) =>
+  element.innerText
+    .trim()
+    .replace(/\s+/g, " ")
+    .replace(/[^\w\s.,!?-]/g, "")
+    .replace(/\s([.,!?-])/g, "$1")
+    .replace(/\s+/g, " ")
+)
+console.log(textContents)
+
 const batch = ["hi", "no", "water"]
 const length = batch.length
 const input = JSON.stringify(batch)
-console.log(input)
-fetchEmbedding(input, length)
+
+const getUmap = async () => {
+  const umap = await fetchEmbedding(input, length)
+  const map = umap.map((vector, index) => {
+    return { sentence: batch[index], vector: vector }
+  })
+  console.log(map)
+}
+
+getUmap()
